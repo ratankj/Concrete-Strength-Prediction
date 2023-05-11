a56a09097e83992214875f251cde565ab8a3198d
ratan kumar jha <imratankj@gamil.com>
2023-05-12 00:14:55 +0530

    data ingestion completed

--- concrete_strength/components/data_ingestion.py
+++ concrete_strength/components/data_ingestion.py
@@ -38,8 +38,9 @@ class DataIngestion:
             logging.info("train test split")
 
             train_set,test_Set = train_test_split(df,test_size=0.20,random_state=42)
-            df.to_csv(self.data_ingestion_config.test_data_path,index=False,header=True)
-            df.to_csv(self.data_ingestion_config.train_data_path,index=False,header=True)
+            train_set.to_csv(self.data_ingestion_config.train_data_path,index=False,header=True)
+            test_Set.to_csv(self.data_ingestion_config.test_data_path,index=False,header=True)
+            
             logging.info("data insgested into raw, train and test data")
 
             return(
