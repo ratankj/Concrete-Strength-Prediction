3d5740655e5a8ce20fdc484acd6740eb8bffd0f9
ratan kumar jha <imratankj@gamil.com>
2023-05-11 21:31:15 +0530

    configuration file

---
+++ concrete_strength/config/configuration.py
@@ -0,0 +1,32 @@
+from concrete_strength.constant import *
+from concrete_strength.exception import CustomException
+from concrete_strength.logger import logging
+
+
+import os,sys
+
+ROOT_DIR = ROOT_DIR_KEY 
+
+DATASET_PATH = os.path.join(ROOT_DIR,DATA_DIR,DATA_DIR_KEY)
+
+
+# DATA INGESTION PATH
+
+RAW_FILE_PATH = os.path.join(ROOT_DIR,ARTIFACT_DIR_KEY,DATA_INGESTION_KEY,CURRENT_TIME_STAMP,RAW_DATA_DIR_KEY)
+
+TEST_FILE_PATH = os.path.join(ROOT_DIR,ARTIFACT_DIR_KEY,DATA_INGESTION_KEY,CURRENT_TIME_STAMP,TEST_DATA_DIR_KEY)
+
+TRAIN_FILE_PATH = os.path.join(ROOT_DIR,ARTIFACT_DIR_KEY,DATA_INGESTION_KEY,CURRENT_TIME_STAMP,TEST_DATA_DIR_KEY)
+
+
+# PREPROCESSING OBJ
+
+
+PREPROCESSING_OBJ_PATH = os.path.join(ROOT_DIR,ARTIFACT_DIR_KEY,
+                                      DATA_TRANSFORMATION_ARTIFACT,CURRENT_TIME_STAMP,
+                                      DATA_TRANSFORMATION_PREPROCESSING_OBJ)
+
+# MODEL FILE
+
+MODEL_FILE_PATH = os.path.join(ROOT_DIR,ARTIFACT_DIR_KEY,
+                               MODEL_ARTIFACT,CURRENT_TIME_STAMP,MODEL_OBJECT)
--- concrete_strength/constant/__init__.py
+++ concrete_strength/constant/__init__.py
@@ -1,4 +1,13 @@
 import os
+from datetime import datetime
+
+
+#to store the log file
+def get_current_time_stamp():
+    return f"{datetime.now().strftime('%Y-%m-%d %H-%M-%S')}"
+
+CURRENT_TIME_STAMP = get_current_time_stamp()
+
 
 ROOT_DIR_KEY = os.getcwd()
 
